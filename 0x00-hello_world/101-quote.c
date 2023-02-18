74e08c678d23046813e04d41ca6592a5b5383400
mohamed11 <rjabmohamed3@gmail.com>
2023-02-17 16:45:36 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 
 {
 	char message[] = "and that piece of art is useful\" - Dora korpar,"
-		"2015-10-19\n";
+		" 2015-10-19\n";
 
 	write(STDERR_FILENO, message, sizeof(message) - 1);
 
