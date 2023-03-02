415a1819e7a565ac3abbc62726300fdca7601090
mohamed11 <rjabmohamed3@gmail.com>
2023-03-02 11:17:37 -0800

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - function that copy a string into another
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: return dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
